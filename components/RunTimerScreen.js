b8c8ffaa14601b0a276f5c37489797487d77510f
rohilverma13 <76930045+rohilverma13@users.noreply.github.com>
2021-01-08 17:38:43 -0600

    disable start when tasks is 0
    RunTimerScreen.js

--- components/RunTimerScreen.js
+++ components/RunTimerScreen.js
@@ -237,7 +237,7 @@ class RunTimerScreen extends Component {
                         <Text style={styles.instructions}>{this.state.currentTaskName}</Text>
 
 
-                        { !this.state.sessionInProgress && this.state.currentTaskName && this.state.currentTaskName.length && this.state.tasks.taskName  >0 &&
+                        {this.state.numberOfTasks > 0 &&
                         <TouchableOpacity style={styles.beginButton} onPress={this.beginSession}>
                             <Text style={styles.colorWhite}>Start</Text>
                         </TouchableOpacity>
