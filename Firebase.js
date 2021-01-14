63f951b91f776ddb826a9a901e7644f9dc0081a1
sidkumar5 <sidharth.usa89@gmail.com>
2021-01-13 18:00:04 -0600

    Revert "Update Firebase.js"
    This reverts commit 73da771cad11fe92a894de6a91a323ad5d9700ad.

--- Firebase.js
+++ Firebase.js
@@ -3,12 +3,12 @@ import firebase from "firebase";import firestore from 'firebase/firestore'
 const settings = {};
 
 const config = {
-  apiKey: "AIzaSyAmIQdENE8ufB2Ai89jEQ1Fc5zZ-rIXO2c",
-  authDomain: "meditationapp-840c3.firebaseapp.com",
-  databaseURL: "https://meditationapp-840c3.firebaseio.com",
-  projectId: "meditationapp-840c3",
-  storageBucket: "meditationapp-840c3.appspot.com",
-  messagingSenderId: "1006085563410",
+  apiKey: "AIzaSyCwNFSRdcSR-dg34hgVtaL25BpvFxMZYMQ",
+  authDomain: "timerapp-b24f8.firebaseapp.com",
+  databaseURL: "https://timerapp-b24f8.firebaseio.com",
+  projectId: "timerapp-b24f8",
+  storageBucket: "timerapp-b24f8.appspot.com",
+  messagingSenderId: "827071971350",
 };
 
 firebase.initializeApp(config);
