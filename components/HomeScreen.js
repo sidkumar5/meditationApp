a754b489efe0087abce2b5e19a7649a9173fd00c
rohilverma13 <76930045+rohilverma13@users.noreply.github.com>
2021-01-15 03:27:29 -0600

    added developer/user buttons

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,11 +1,68 @@
-import React, { Component } from 'react';
-import { StyleSheet, ScrollView, ActivityIndicator, View, Text , TouchableOpacity} from 'react-native';
-import {  ListItem, Button, Avatar } from 'react-native-elements';
-import firebase from '../Firebase';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  ScrollView,
+  ActivityIndicator,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import { ListItem, Button, Avatar } from "react-native-elements";
+import firebase from "../Firebase";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-class HomeScreen extends Component{
+class HomeScreen extends Component {
+  static navigationOptions = ({ navigation }) => {
+    return {};
+  };
 
+  constructor(props) {
+    super(props);
+    this.unsubscribe = null;
+    this.state = {
+      isLoading: true,
+    };
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    this.setState({ isLoading: false });
+  };
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          style={styles.button}
+          title="Developer Interface"
+          onPress={() => {
+            this.props.navigation.navigate("Timer");
+          }}
+        />
+        <Button
+          style={styles.button}
+          title="User InterFace"
+          onPress={() => {
+            this.props.navigation.navigate("uTimer");
+          }}
+        />
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  button: { padding: 20 },
+  detailButton: {
+    marginTop: 10,
+    flexDirection: "row",
+  },
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    //justifyContent: "space-between",
+  },
+});
 
 export default HomeScreen;
